7c5841ea916a676f33fc8c9a5f5fb2c3a26d6990
AtharvGhumtane <atharvghumtane02@gmail.com>
2026-07-13 18:48:38 +0530

    Remove offline_access from frontend oauth scope to resolve Keycloak error

--- fitness-app-frontend/src/authConfig.js
+++ fitness-app-frontend/src/authConfig.js
@@ -3,7 +3,7 @@ export const authConfig = {
     authorizationEndpoint: 'http://localhost:8070/realms/fitness-oauth2/protocol/openid-connect/auth',
     tokenEndpoint: 'http://localhost:8070/realms/fitness-oauth2/protocol/openid-connect/token',
     redirectUri: 'http://localhost:5173/',
-    scope: 'openid profile email offline_access',
+    scope: 'openid profile email',
     onRefreshTokenExpire: (event) => event.logIn(),
     autoLogin: false,
     decodeToken: true,
